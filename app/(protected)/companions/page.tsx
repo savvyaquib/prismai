3035047a12b26a42dd46f18ca071df98f625977f
savvyaquib <itsmohammadaquib@gmail.com>
2026-07-28 22:50:38 +0530

    feat: implement initial CompanionsLibrary page with filtered companion fetching

--- app/(protected)/companions/page.tsx
+++ app/(protected)/companions/page.tsx
@@ -1,8 +1,13 @@
+import { getAllCompanions } from "@/lib/actions/companion.actions";
 
-export const CompanionsLibrary = () => {
-  return (
-    <div>Companion</div>
-  )
-}
+export const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
 
-export default CompanionsLibrary    
+  const companions = await getAllCompanions({ subject, topic });
+  console.log(companions);
+  return <div>Companions Library </div>;
+};
+
+export default CompanionsLibrary;
