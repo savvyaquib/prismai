9c688106d880c2d5220c867a925e934ae418a679
savvyaquib <itsmohammadaquib@gmail.com>
2026-07-29 16:34:23 +0530

    feat: implement companions library page with filtered list and companion cards

--- app/(protected)/companions/page.tsx
+++ app/(protected)/companions/page.tsx
@@ -1,4 +1,6 @@
+import CompanionCard from "@/components/CompanionCard";
 import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
 export const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
   const filters = await searchParams;
@@ -7,7 +9,19 @@ export const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
 
   const companions = await getAllCompanions({ subject, topic });
   console.log(companions);
-  return <div>Companions Library </div>;
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))}
+      </section>
+    </main>
+  ); 
 };
 
 export default CompanionsLibrary;
