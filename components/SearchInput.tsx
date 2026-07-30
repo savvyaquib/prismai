65a663001e1fbbaa4c1e7e1270c8291fcbc3cd3a
savvyaquib <itsmohammadaquib@gmail.com>
2026-07-30 10:13:07 +0530

    feat: add SearchInput and SubjectFilter components with URL query state management

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -33,7 +33,7 @@ const SearchInput = () => {
                     router.push(newUrl, { scroll: false });
                 }
             }
-        }, 500)
+        }, 1000)
     }, [searchQuery, router, searchParams, pathname]);
 
     return (
