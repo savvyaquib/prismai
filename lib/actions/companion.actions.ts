1ff0803988764d1c1183a1b61d94973765cc0b43
savvyaquib <itsmohammadaquib@gmail.com>
2026-08-06 17:31:27 +0530

    feat: add subscription page and implement companion management server actions

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -108,9 +108,39 @@ export const getUserCompanions = async (userId: string) => {
   const { data, error } = await supabase
     .from("companions")
     .select()
-    .eq("author", userId)
+    .eq("author", userId);
 
   if (error) throw new Error(error.message);
 
-  return data
+  return data;
+};
+
+export const newCompanionPermissions = async () => {
+  const { userId, has } = await auth();
+  const supabase = createSupabaseClient();
+
+  let limit = 0;
+
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ feature: "3_companion_limit" })) {
+    limit = 3;
+  } else if (has({ feature: "10_companion_limit" })) {
+    limit = 10;
+  }
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+
+  if (error) throw new Error(error.message);
+
+  const companionCount = data?.length;
+
+  if (companionCount >= limit) {
+    return false;
+  } else {
+    return true;
+  }
 };
